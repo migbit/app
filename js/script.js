668f3b6bc67a5a37a6741c4f8a854bdd05fc0746
migbit <38838197+migbit@users.noreply.github.com>
2024-10-06 13:30:14 +0100

    mail test

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 // js/script.js
 
-// Importar as funções necessárias do Firebase e EmailJS
+// Importar as funções necessárias do Firebase
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
-import emailjs from "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
 
 // Configuração do Firebase
 const firebaseConfig = {
@@ -42,13 +41,6 @@ function copiarMensagem(texto) {
 
 // Exportar a função se necessário
 export { copiarMensagem };
-
-// Inicializar EmailJS
-(function() {
-    emailjs.init({
-        publicKey: "dRbsNarrwt7bsIiDK" // Use your actual public key here
-    });
-})();
 
 // Função para enviar um e-mail de urgência usando EmailJS
 function enviarEmailUrgencia() {
