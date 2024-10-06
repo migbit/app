f955dec76fb29d55096ef3b99655f6dca72fff5e
migbit <38838197+migbit@users.noreply.github.com>
2024-10-06 23:45:21 +0100

    Update faturas.js

--- js/faturas.js
+++ js/faturas.js
@@ -335,7 +335,7 @@ window.exportarPDFFaturacao = function(key, grupoJson) {
 
         // Cabeçalho da Tabela em negrito e centralizado
         let yPosition = 30;
-        const xPositions = [10, 50, 90, 130, 170]; // Ajustado para começar mais perto da margem esquerda
+        const xPositions = [5, 40, 80, 130, 170]; // Ajustado para começar mais perto da margem esquerda
         const colWidths = [40, 40, 40, 40, 40]; // Larguras das colunas
         doc.setFontSize(12);
         doc.setFont("helvetica", "bold");
