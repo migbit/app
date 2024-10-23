63985d698ac3e6a43d2c664d7589b1d5c7c688e3
migbit <38838197+migbit@users.noreply.github.com>
2024-10-23 19:11:13 +0100

    app

--- sw.js
+++ sw.js
@@ -1,51 +1,67 @@
 // Define a cache name
 const CACHE_NAME = 'app-cache-v1';
 
-// Files to cache
+// Files to cache (all needed assets)
 const ASSETS_TO_CACHE = [
-    './',  // Relative paths
-    './index.html',
-    './css/styles.css',
+    './',  // The root index.html
+    './index.html',  // Main HTML
+    './css/styles.css',  // CSS styles
+    './js/caixa.js',  // JavaScript files
+    './js/cenas.js',
+    './js/compras.js',
+    './js/faturas.js',
+    './js/mensagens.js',
+    './js/reparações.js',
     './js/script.js',
-    './favicon.ico',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+    './js/tarefas.js',
+    './favicon.ico',  // Favicon
+    './icons/icon-192.png',  // Icons for PWA
+    './icons/icon-512.png',
+    './modules/caixa.html',  // Other HTML pages in the "modules" folder
+    './modules/cenas.html',
+    './modules/compras.html',
+    './modules/faturas.html',
+    './modules/ficheiros.html',
+    './modules/mensagens.html',
+    './modules/reparações.html',
+    './modules/tarefas.html',
+    './maps/123.jpg',  // Images from the "maps" folder
+    './maps/1248.jpg',
+    './modules/mensagensData.json'  // JSON file for data
 ];
 
 // Install event: Cache all the necessary assets
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
 });
 
-// Fetch event: Serve files from the cache first, then from the network
+// Fetch event: Serve files from cache first, then from network
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Return cached version, or fetch from the network if not in cache
-        return response || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
 });
 
-// Activate event: Clean up old caches if necessary
+// Activate event: Clean up old caches
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
